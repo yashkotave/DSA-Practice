3991de863a91afe4e0f6eba91789d28f5da2b134
Yash Kotave <0176cs231228@lnct.ac.in>
2026-03-19 13:36:56 +0530

    Time: 3 ms (87.45%), Space: 43.3 MB (53.29%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,8 +1,34 @@
+import java.util.*;
+
 class Solution {
-        public boolean isValid(String s) {
-                while (s.contains("()") || s.contains("{}") || s.contains("[]")) {
-                            s = s.replace("()", "").replace("{}", "").replace("[]", "");
-                                    }
-                                                  return s.isEmpty();
-                                                      }
-                                                      }
+    public boolean isValid(String str) {
+
+        Stack<Character> st = new Stack<>();
+
+        for(int i = 0; i < str.length(); i++) {
+
+            char ch = str.charAt(i);
+
+            // opening brackets
+            if(ch == '(' || ch == '[' || ch == '{') {
+                st.push(ch);
+            } 
+            else {
+                if(st.isEmpty()) return false;
+
+                char top = st.peek();
+
+                if((top == '(' && ch == ')') ||
+                   (top == '{' && ch == '}') ||
+                   (top == '[' && ch == ']')) {
+
+                    st.pop();
+                } else {
+                    return false;
+                }
+            }
+        }
+
+        return st.isEmpty(); // correct final check
+    }
+}
