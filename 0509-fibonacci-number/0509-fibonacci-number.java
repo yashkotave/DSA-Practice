8ab098c44668764a30f20a9fcc5fa9710136001f
Yash Kotave <0176cs231228@lnct.ac.in>
2026-03-20 10:27:44 +0530

    Time: 0 ms (100%), Space: 42.2 MB (46.35%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -2,6 +2,11 @@ class Solution {
     public int fib(int n) {
         if(n<=0) return n ;
         int a = 0 ; int b= 1;
-        
+        for( int i = 2; i<=n; i++){
+            int temp = a + b;
+            a = b;
+            b= temp;
+        }
+        return b;
     }
 }
