8c46538cda318004c0756a1e3bb41ddac103b3ec
Yash Kotave <0176cs231228@lnct.ac.in>
2026-03-26 21:07:28 +0530

    Time: 1 ms (90.45%), Space: 42.6 MB (65.63%) - LeetHub

--- 0342-power-of-four/0342-power-of-four.java
+++ 0342-power-of-four/0342-power-of-four.java
@@ -1,10 +1,5 @@
 class Solution {
     public boolean isPowerOfFour(int n) {
-        // Keep dividing n by 4 while it's divisible
-        while (n > 1 && n % 4 == 0) {
-            n /= 4;
-        }
-        // If n becomes 1, it's a power of 4
-        return n == 1;
+            return ((n>0) && (n&n-1)==0) && (n-1) %3 ==0;
     }
 }
