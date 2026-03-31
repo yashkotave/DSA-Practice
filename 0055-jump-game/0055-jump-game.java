4676cec0ec4d019321768aabc5ca8d093549800a
Yash Kotave <0176cs231228@lnct.ac.in>
2026-03-31 19:37:13 +0530

    Time: 2 ms (85.8%), Space: 47.8 MB (71.81%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,11 +1,13 @@
 class Solution {
     public boolean canJump(int[] nums) {
-        int maxIdx = 0;
-        for( int i = 0; i< nums.length; i++){
-            if( i> maxIdx) return false;
-
-            maxIdx = Math.max(maxIdx,i+nums[i]);
+      int maxReach = 0;
+      for( int i =0 ;i<nums.length; i++){
+        if(i>maxReach){
+            return false;
         }
-        return true;
+
+        maxReach = Math.max(maxReach,i+nums[i]);
+      }
+      return true;
     }
 }
