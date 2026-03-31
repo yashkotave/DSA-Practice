0107c05acf999c8dfba7f184170edf917fd88d9a
Yash Kotave <0176cs231228@lnct.ac.in>
2026-03-31 10:20:52 +0530

    Time: 1 ms (99.84%), Space: 55.9 MB (26.16%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -2,7 +2,10 @@ class Solution {
     public int majorityElement(int[] nums) {
         int candidate = 0;
         int count = 0;
-
+// Ek candidate aur count maintain karo.
+// Same element mile → count++
+// Different mile → count--
+// Count 0 → new candidate
         for (int i = 0; i < nums.length; i++) {
 
             if (count == 0) {
