1af51b1c68de72ef7e0da1e38a722778fef23a22
Yash Kotave <0176cs231228@lnct.ac.in>
2026-04-10 00:14:56 +0530

    Time: 0 ms (100%), Space: 43.1 MB (58.31%) - LeetHub

---
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int lengthOfLastWord(String s) {
+        int ans = 0;
+        for( int i = s.length()-1 ; i>=0;i--){
+            if(s.charAt(i)==' ' && ans>0) break;
+            else if(s.charAt(i)!=' ') ans++;
+        }
+        return ans;
+    }
+}
