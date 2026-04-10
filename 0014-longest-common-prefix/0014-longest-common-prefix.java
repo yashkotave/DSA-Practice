376b3d61f744d70f4e2118359917210661108bc2
Yash Kotave <0176cs231228@lnct.ac.in>
2026-04-10 11:06:56 +0530

    Time: 2 ms (17.79%), Space: 43.5 MB (11.27%) - LeetHub

---
+++ 0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -0,0 +1,24 @@
+class Solution {
+    public String Common(String s1,String s2){
+        int n = Math.min(s1.length(),s2.length());
+        StringBuilder sb = new StringBuilder();
+
+        for( int i =0; i<n; i++){
+            if(s1.charAt(i)==s2.charAt(i))
+            sb.append(s1.charAt(i));
+            else
+            break;
+        }
+
+        return sb.toString();
+
+    }
+    public String longestCommonPrefix(String[] strs) {
+        String res=strs[0];
+
+        for( int i = 1; i<strs.length; i++){
+            res = Common(res,strs[i]);
+        }
+        return res;
+    }
+}
