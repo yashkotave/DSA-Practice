6cdfca6d8d62c9bf354c7618c3f62efa270fcb70
Yash Kotave <0176cs231228@lnct.ac.in>
2026-04-21 00:16:47 +0530

    Time: 26 ms (97.36%), Space: 143.4 MB (92.75%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.java
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.java
@@ -10,45 +10,24 @@ class Solution {
         int[] result = new int[n-k+1];
 
         //step 2 Setup deque for the first K elements
-
-
         for( int i = 0; i<k; i++){
-
-            //remove all smaller elements from the back
-            while(!deque.isEmpty() && nums[deque.peekLast()]<= nums[i]){
-
+            while(!deque.isEmpty() && nums[deque.peekLast()]<=nums[i]){
                 deque.pollLast();
-
             }
             deque.offerLast(i);
-
         }
+        result[0] =  nums[deque.peekFirst()];
 
-        //the front of the deque is the max of the first window
-        result[0] = nums[deque.peekFirst()];
-
-        // step 3  process the remaining elements;
-
-        for ( int i = k; i<n; i++){
-            
-            //remove the element that has slid out of  the window
-
-            if(deque.peekFirst() <=i-k) {
+        for ( int i = k ; i<n;i++){
+            if(deque.peekFirst()<=i-k){
                 deque.pollFirst();
             }
+        while(!deque.isEmpty() && nums[deque.peekLast()]<=nums[i]){
+            deque.pollLast();
+        }
+        deque.offerLast(i);
 
-            //remove all the elements smaller than the incoming elemnet
-
-            while(!deque.isEmpty() && nums[deque.peekLast()]<= nums[i] ){
-                deque.pollLast();
-
-            }
-
-            //add current elemnent index;
-            deque.offerLast(i);
-
-            //the front of the dequeu is always the max of the window
-            result[i-k+1] = nums[deque.peekFirst()];
+        result[i-k+1] = nums[deque.peekFirst()];
         }
 
         return result;
