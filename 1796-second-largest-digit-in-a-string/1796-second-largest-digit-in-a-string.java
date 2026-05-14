42359e242441c1678691a2b209b984528a8b1c2a
Yash Kotave <0176cs231228@lnct.ac.in>
2026-05-14 11:26:50 +0530

    Time: 9 ms (6.85%), Space: 44.7 MB (7.37%) - LeetHub

--- 1796-second-largest-digit-in-a-string/1796-second-largest-digit-in-a-string.java
+++ 1796-second-largest-digit-in-a-string/1796-second-largest-digit-in-a-string.java
@@ -1,13 +1,19 @@
 class Solution {
     public int secondHighest(String s) {
-        int count = 1;
-        for (char c = '9'; c >= '0'; c--) {
-            if (s.indexOf(c) != -1) {
-                if (count-- <= 0) {
-                    return c - '0';
-                }
+        char[] digits = s.replaceAll("[^0-9]", "").toCharArray();
+        int firstMax = -1;
+        int secondMax = -1;
+
+        for(int i = 0; i < digits.length; i++){
+           
+            int currNum = digits[i] - '0';
+            if(firstMax < currNum){
+                secondMax = firstMax;
+                firstMax = currNum;
+            } else if (currNum < firstMax && currNum > secondMax) {
+                secondMax = currNum;
             }
         }
-        return -1;
+        return secondMax;
     }
 }
