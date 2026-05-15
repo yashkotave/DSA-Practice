c86792bd8e3cb29824c5d2ea3621bf75baf4dbde
Yash Kotave <0176cs231228@lnct.ac.in>
2026-05-15 09:49:32 +0530

    Time: 4 ms (82.15%), Space: 46.9 MB (5.05%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -2,27 +2,31 @@ import java.util.Arrays;
 
 class Solution {
     public boolean isAnagram(String s, String t) {
-       HashMap<Character,Integer> map = new HashMap<>();
-       for (int i =0; i<s.length(); i++){
-        char ch= s.charAt(i);
-        map.put(ch,map.getOrDefault(ch,0)+1);
-       }
+    //    HashMap<Character,Integer> map = new HashMap<>();
+    //    for (int i =0; i<s.length(); i++){
+    //     char ch= s.charAt(i);
+    //     map.put(ch,map.getOrDefault(ch,0)+1);
+    //    }
 
-       for(int i =0; i<t.length(); i++){
-        char ch = t.charAt(i);
-        if(map.get(ch) != null){
-            if(map.get(ch) == 1){
-                map.remove(ch);
-            }else{
-                map.put(ch,map.get(ch)-1);
-            }
-        }else{
-            return false;
-        }
-       }
+    //    for(int i =0; i<t.length(); i++){
+    //     char ch = t.charAt(i);
+    //     if(map.get(ch) != null){
+    //         if(map.get(ch) == 1){
+    //             map.remove(ch);
+    //         }else{
+    //             map.put(ch,map.get(ch)-1);
+    //         }
+    //     }else{
+    //         return false;
+    //     }
+    //    }
+    //    return map.isEmpty();
+    char[] schars  = s.toCharArray();
+    char[] tchars = t.toCharArray();
+    Arrays.sort(schars);
+    Arrays.sort(tchars);
 
-       return map.isEmpty();
+    return Arrays.equals(schars,tchars);
     }
-
 }
 
