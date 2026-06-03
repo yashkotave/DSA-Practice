3a71b3b993863126f8c6ed040161b529f721d99e
Yash Kotave <0176cs231228@lnct.ac.in>
2026-06-04 00:26:06 +0530

    Time: 0 ms (100%), Space: 43.8 MB (59.04%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -4,12 +4,16 @@ class Solution {
         int j = n-1;
         int k = m+n-1;
 
-        while(i>=0 && j>=0){
-            if(nums1[i]>nums2[j]){
-                nums1[k--] = nums1[i--];
+        while(j>=0){
+            if(i>=0 && nums1[i]>nums2[j]){
+                nums1[k] = nums1[i];
+                k--;
+                i--;
             }
             else{
-                nums1[k--] = nums2[j--];
+                nums1[k] = nums2[j];
+                k--;
+                j--;
             }
         }
 
