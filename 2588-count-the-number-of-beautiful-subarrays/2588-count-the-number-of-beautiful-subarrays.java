71261f5fa7d7155f126aa72d53453e60ee1e6927
Yash Kotave <0176cs231228@lnct.ac.in>
2026-06-10 17:20:02 +0530

    Time: 67 ms (9.85%), Space: 118.8 MB (9.85%) - LeetHub

--- 2588-count-the-number-of-beautiful-subarrays/2588-count-the-number-of-beautiful-subarrays.java
+++ 2588-count-the-number-of-beautiful-subarrays/2588-count-the-number-of-beautiful-subarrays.java
@@ -1,14 +1,35 @@
 class Solution {
     public long beautifulSubarrays(int[] nums) {
-        HashMap<Long,Long> map = new HashMap<>();
-        long xor =0;
-        long count =0;
-        map.put(0L,1L);
-        for(int num : nums){
+
+        HashMap<Long, Long> map = new HashMap<>();
+
+        long xor = 0;
+        long count = 0;
+
+        // Step 1:
+        // XOR = 0 occurs once before starting
+        map.put(0L, 1L);
+
+        // Step 2:
+        // Traverse array
+        for (int num : nums) {
+
+            // Step 3:
+            // Calculate prefix XOR
             xor ^= num;
-            count+= map.getOrDefault(xor,0L);
-            map.put(xor,map.getOrDefault(xor,0L)+1);
+
+            // Step 4:
+            // If same XOR occurred before,
+            // then subarray XOR = 0
+            count += map.getOrDefault(xor, 0L);
+
+            // Step 5:
+            // Store current XOR frequency
+            map.put(xor, map.getOrDefault(xor, 0L) + 1);
         }
+
+        // Step 6:
+        // Return total beautiful subarrays
         return count;
     }
 }
