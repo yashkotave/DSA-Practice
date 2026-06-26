2a973bd5e140822503f16dfbd392bbe4c3b11f61
Yash Kotave <0176cs231228@lnct.ac.in>
2026-06-26 16:44:50 +0530

    Time: 1 ms (99.91%), Space: 43.4 MB (8.47%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.java
+++ 0389-find-the-difference/0389-find-the-difference.java
@@ -2,10 +2,12 @@ class Solution {
     public char findTheDifference(String s, String t) {
         char ans= 0;
         for(char ch :s.toCharArray()){
-            ans^=ch;
+           ans = (char)(ans^ch);
+           // ans^=ch;
         }
         for(char ch : t.toCharArray()){
-            ans^=ch;
+           // ans^=ch;
+             ans = (char)(ans^ch);
         }
         return ans;
     }
