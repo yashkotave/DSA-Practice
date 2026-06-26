bf37db303ece779451a57dff3a6baf0d16b61e14
Yash Kotave <0176cs231228@lnct.ac.in>
2026-06-26 16:39:01 +0530

    Time: 0 ms (100%), Space: 42.3 MB (92.93%) - LeetHub

--- 3492-maximum-containers-on-a-ship/3492-maximum-containers-on-a-ship.java
+++ 3492-maximum-containers-on-a-ship/3492-maximum-containers-on-a-ship.java
@@ -1,7 +1,20 @@
 class Solution {
     public int maxContainers(int n, int w, int maxWeight) {
-        int totalContainers = n * n;
-        int weightAllowed = maxWeight / w;
-        return Math.min(totalContainers, weightAllowed);
-    }
+        //limit  
+        n = n*n;
+
+        //2*2 =4
+        //4*3 = 12<15 
+        int weight = n*w;
+
+        if(weight <= maxWeight ) return n;
+       //return n;
+
+        //2nd test 
+        // n = 3*3;
+        //overflow 9*5 >20
+        //so divide by 20/5
+
+       else return  maxWeight/w;
+    }  
 }
