ff9eef184d45092eb93cab0866bbb8c533803183
Yash Kotave <0176cs231228@lnct.ac.in>
2026-06-28 13:46:51 +0530

    Time: 3 ms (99.58%), Space: 79.4 MB (5%) - LeetHub

--- 1846-maximum-element-after-decreasing-and-rearranging/1846-maximum-element-after-decreasing-and-rearranging.java
+++ 1846-maximum-element-after-decreasing-and-rearranging/1846-maximum-element-after-decreasing-and-rearranging.java
@@ -1,10 +1,28 @@
 class Solution {
     public int maximumElementAfterDecrementingAndRearranging(int[] arr) {
-       Arrays.sort(arr);
+      // TC o(NlogN) Brute force
+      /* Arrays.sort(arr);
        arr[0] = 1;
        for(int i =1; i<arr.length; i++){
         arr[i] = Math.min(arr[i],arr[i-1]+1);
        } 
-       return arr[arr.length-1];
+       return arr[arr.length-1];*/
+       
+       //optimzed o(N) but space o(N);
+
+       int n = arr.length;
+       int[] count = new int[n+1];
+
+        // Values greater than n can be treated as n
+       for(int num : arr){
+        count[Math.min(num,n)]++;
+       }
+       int ans = 0;
+       //build the maximum valid sequence
+
+       for(int i =1; i<=n; i++){
+        ans = Math.min(ans+count[i],i);
+       }
+       return ans;
     }
 }
