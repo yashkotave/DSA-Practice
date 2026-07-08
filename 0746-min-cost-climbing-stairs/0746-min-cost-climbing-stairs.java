6f94e79efaaba5c91a51c5c15adc791d5471b841
Yash Kotave <0176cs231228@lnct.ac.in>
2026-07-08 14:27:27 +0530

    Time: 0 ms (100%), Space: 45 MB (40.77%) - LeetHub

---
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int minCostClimbingStairs(int[] cost) {
+        int  n = cost.length;
+        int[] dp = new int[n+2];
+        for(int i = n-1 ; i>=0; i--){
+            dp[i] = cost[i] + Math.min(dp[i+1],dp[i+2]);
+        }
+        return Math.min(dp[0],dp[1]);
+    }
+}
