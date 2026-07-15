b376e42a0fd51c8ad906d535d08a36fff290b72c
Yash Kotave <0176cs231228@lnct.ac.in>
2026-07-15 11:23:08 +0530

    Time: 1 ms (83.11%), Space: 42.6 MB (57.16%) - LeetHub

--- 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.java
+++ 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.java
@@ -1,5 +1,19 @@
 class Solution {
     public int gcdOfOddEvenSums(int n) {
-      return n;
+        // Direct sum formulas
+        int o = n * n;       
+        int e = n * (n + 1); 
+        
+        return gcd(o, e); 
+    }
+    
+    // Helper method for GCD
+    private int gcd(int a, int b) {
+        while (b != 0) {
+            int t = b;
+            b = a % b;
+            a = t;
+        }
+        return a;
     }
 }
