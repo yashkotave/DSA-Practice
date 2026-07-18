753f18f1d8a763e860de7f54b1e440f520b6e4ae
Yash Kotave <0176cs231228@lnct.ac.in>
2026-07-18 15:51:32 +0530

    Time: 1 ms (99.99%), Space: 43.2 MB (57.58%) - LeetHub

--- 0409-longest-palindrome/0409-longest-palindrome.java
+++ 0409-longest-palindrome/0409-longest-palindrome.java
@@ -1,31 +1,20 @@
 class Solution {
     public int longestPalindrome(String s) {
-        HashMap<Character,Integer> map = new HashMap <>();
-        //step 1 Count frequency
+       int [] freq = new int[128];
+       for(char ch: s.toCharArray()){
+        freq[ch]++;
+       }
+       int length= 0;
 
-        for (int i =0; i<s.length(); i++){
-            char ch = s.charAt(i);
-            map.put(ch,map.getOrDefault(ch,0)+1);
-        }
+       for(int count : freq){
+        if(count%2 ==0) length += count;
+        else length += count-1;
 
-        int length =0;
-        boolean oddFound = false;
+       }
+       if(length<s.length()) length++;
 
-        //step 2 calculate maximum palindrome length
-        for(int freq  : map.values()){
-            if(freq%2 ==0 ){
-                length = length+freq;
-            }else{
-                length = length + freq -1;
-                oddFound = true;
-            }
-        }
-
-        //Step 3 ONE odd character can be placed in the ceentre
-
-        if(oddFound){
-            length= length+1;   
-        }
-        return length;
+       return length;
     }
+
+
 }
