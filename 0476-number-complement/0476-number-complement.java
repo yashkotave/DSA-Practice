b701100f17b8cffb11dd4f85fd105ef15ced3343
Yash Kotave <0176cs231228@lnct.ac.in>
2026-07-22 22:49:57 +0530

    Time: 0 ms (100%), Space: 42.1 MB (59.37%) - LeetHub

--- 0476-number-complement/0476-number-complement.java
+++ 0476-number-complement/0476-number-complement.java
@@ -1,11 +1,18 @@
 class Solution {
     public int findComplement(int num) {
-        if (num == 0) return 1;
+        //0(n) space for inbuilt ke liye
+        //0(logn) for mask
+        // if (num == 0) return 1;
+        // int bitLength = Integer.toBinaryString(num).length();
+        // int mask = (1 << bitLength) - 1;
+        // return num ^ mask;
 
-        int bitLength = Integer.toBinaryString(num).length();
-        
-        int mask = (1 << bitLength) - 1;
-        
-        return num ^ mask;
+
+        //o(1) space time o(logn);
+        int mask  = 1;
+        while(mask<num){
+            mask = (mask<<1)|1;
+        }
+        return num^mask;
     }
 }
