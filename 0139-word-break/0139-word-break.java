ac511e11fd5d8f13876b2b7104355f31fffe5e15
Yash Kotave <0176cs231228@lnct.ac.in>
2026-08-20 08:53:11 +0530

    Time: 8 ms (49.67%), Space: 46.3 MB (22.77%) - LeetHub

---
+++ 0139-word-break/0139-word-break.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean wordBreak(String s, List<String> wordDict) {
+       if (s == null || s.length() == 0) {
+            return true;
+        }
+
+        Set<String> dict = new HashSet<>(wordDict);
+        int n = s.length();
+
+        // dp[i] represents whether s[0...i-1] can be segmented
+        boolean[] dp = new boolean[n + 1];
+        dp[0] = true; // Base case: empty string
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 0; j < i; j++) {
+                // If s[0...j-1] is valid and s[j...i-1] is in dictionary
+                if (dp[j] && dict.contains(s.substring(j, i))) {
+                    dp[i] = true;
+                    break; // Move to next i
+                }
+            }
+        }
+
+        return dp[n]; 
+    }
+}
