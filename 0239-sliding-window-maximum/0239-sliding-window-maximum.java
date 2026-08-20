61c78e2e60e5506549695fb7f8c1d85a4c292b6e
Yash Kotave <0176cs231228@lnct.ac.in>
2026-08-20 18:49:04 +0530

    Time: 30 ms (73.52%), Space: 143.4 MB (89.09%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.java
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.java
@@ -1,35 +1,25 @@
 class Solution {
     public int[] maxSlidingWindow(int[] nums, int k) {
         int n = nums.length;
+        int[] ans = new int[n - k + 1];
+        Deque<Integer> dq = new ArrayDeque<>();
 
-
-        //initialize the dequeu and result arrray
-        //deque store indices not value;
-
-        Deque<Integer> deque = new ArrayDeque<>();
-        int[] result = new int[n-k+1];
-
-        //step 2 Setup deque for the first K elements
-        for( int i = 0; i<k; i++){
-            while(!deque.isEmpty() && nums[deque.peekLast()]<=nums[i]){
-                deque.pollLast();
+        for (int i = 0; i < n; i++) {
+            if (!dq.isEmpty() && dq.peekFirst() == i - k) {
+                dq.pollFirst();
             }
-            deque.offerLast(i);
-        }
-        result[0] =  nums[deque.peekFirst()];
 
-        for ( int i = k ; i<n;i++){
-            if(deque.peekFirst()<=i-k){
-                deque.pollFirst();
+            while (!dq.isEmpty() && nums[dq.peekLast()] <= nums[i]) {
+                dq.pollLast();
             }
-        while(!deque.isEmpty() && nums[deque.peekLast()]<=nums[i]){
-            deque.pollLast();
-        }
-        deque.offerLast(i);
 
-        result[i-k+1] = nums[deque.peekFirst()];
+            dq.offerLast(i);
+
+            if (i >= k - 1) {
+                ans[i - k + 1] = nums[dq.peekFirst()];
+            }
         }
 
-        return result;
+        return ans;
     }
 }
