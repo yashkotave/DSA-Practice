670a2fad4d93b178b38e850897f09863922c31eb
Yash Kotave <0176cs231228@lnct.ac.in>
2026-08-20 09:26:36 +0530

    Time: 1 ms (99.3%), Space: 43.2 MB (86.78%) - LeetHub

--- 0139-word-break/0139-word-break.java
+++ 0139-word-break/0139-word-break.java
@@ -5,6 +5,12 @@ class Solution {
         }
 
         Set<String> dict = new HashSet<>(wordDict);
+
+        int maxLen =0;
+        for(String word : wordDict){
+            dict.add(word);
+            maxLen = Math.max(word.length(),maxLen);
+        }
         int n = s.length();
 
         // dp[i] represents whether s[0...i-1] can be segmented
@@ -12,7 +18,8 @@ class Solution {
         dp[0] = true; // Base case: empty string
 
         for (int i = 1; i <= n; i++) {
-            for (int j = 0; j < i; j++) {
+            int minj = Math.max(0,i-maxLen);
+            for (int j = i-1; j >= minj; j--) {
                 // If s[0...j-1] is valid and s[j...i-1] is in dictionary
                 if (dp[j] && dict.contains(s.substring(j, i))) {
                     dp[i] = true;
