99d934ac5942e563689631079d745532df6c50cd
Yash Kotave <0176cs231228@lnct.ac.in>
2026-08-21 12:08:33 +0530

    Time: 0 ms (100%), Space: 44.7 MB (33.64%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.java
+++ 0031-next-permutation/0031-next-permutation.java
@@ -1,24 +1,37 @@
 class Solution {
-    public void nextPermutation(int[] nums) {
-        int i = nums.length - 2;
-        while (i >= 0 && nums[i] >= nums[i + 1]) i--;
-        
+    static void nextPermutation(int[] nums) {
+        int n = nums.length;
+        int i = n - 2;
+
+        // Step 1: Find the pivot element
+        while (i >= 0 && nums[i] >= nums[i + 1]) {
+            i--;
+        }
+
         if (i >= 0) {
-            int j = nums.length - 1;
-            while (nums[j] <= nums[i]) j--;
+            // Step 2: Find element just greater than pivot
+            int j = n - 1;
+            while (nums[j] <= nums[i]) {
+                j--;
+            }
             swap(nums, i, j);
         }
-        reverse(nums, i + 1);
+
+        // Step 3: Reverse the suffix
+        reverse(nums, i + 1, n - 1);
     }
-    
-    private void swap(int[] nums, int i, int j) {
+
+    static void swap(int[] nums, int i, int j) {
         int temp = nums[i];
         nums[i] = nums[j];
         nums[j] = temp;
     }
-    
-    private void reverse(int[] nums, int start) {
-        int end = nums.length - 1;
-        while (start < end) swap(nums, start++, end--);
+
+    static void reverse(int[] nums, int start, int end) {
+        while (start < end) {
+            swap(nums, start, end);
+            start++;
+            end--;
+        }
     }
 }
