b54e8ef5c221eae70736f0292968e7035eb7b8b4
Yash Kotave <0176cs231228@lnct.ac.in>
2026-08-23 10:37:12 +0530

    Time: 1 ms (74.49%), Space: 43.2 MB (20.41%) - LeetHub

---
+++ 1024-video-stitching/1024-video-stitching.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int videoStitching(int[][] clips, int time) {
+      if(time ==0)return 0;
+      if(clips == null  || clips.length ==0 )return -1;
+      // Sort: start ascending; if start is equal, end descending
+
+      Arrays.sort(clips,(a,b)->{
+        if(a[0] != b[0] ){
+            return Integer.compare(a[0],b[0]);
+        }
+        return Integer.compare(a[1],b[1]);
+
+      });
+      int maxReach =0;
+      int totalClips = 0;
+      int currEnd = 0;
+      int i =0;
+      int n = clips.length;
+      while(currEnd< time){
+        while(i<n && clips[i][0]<= currEnd){
+            maxReach = Math.max(maxReach,clips[i][1]);
+            i++;
+        }
+        if(maxReach<= currEnd){
+            return -1;
+        }   
+        currEnd = maxReach;
+        totalClips++;
+
+      }
+      return totalClips;
+    }
+}
